1b531bceb5d943e61b79a2618cf9a70445710055
Mumtaz Khan <mumtzkhan7@gmail.com>
2017-12-15 05:10:36 +0000

    fixed courses/dashboard

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,20 +1,6 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_lesson, only: [:show]
- 
- def show
- end
 
- private
-
- def require_authorized_for_current_lesson
-      if !current_user.enrolled_in?(current_lesson.section.course)
-      redirect_to current_lesson.section.course, alert: 'You must be enrolled to view lessons!'
-    end
-  end
-
- helper_method :current_lesson
- def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
+  def index
   end
 end
